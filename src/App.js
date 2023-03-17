ada4cb0240295921d68f7d73cb97c73046f0f7ba
Kevin Gu <kevingu2@illinois.edu>
2023-03-17 10:59:34 -0500

    Resizing compatibility

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ function App() {
         <div>
           <h1>about us</h1>
         </div>
-        <AboutCard layout="left" content="this is content" header="we are a market risk insurance firm"></AboutCard>
-        <AboutCard layout="right" content="this is content" header="we are a market risk insurance firm"></AboutCard>
+        <AboutCard layout="left" content="We treat selling options as a market risk insurance policy. When someone sells an option, they are essentially agreeing to take on the risk of the underlying asset moving in a certain direction. In exchange for taking on this reisk, the seller receives a premium from the buyer of the option. This is similar to an insurance contract." header="we are a market risk insurance firm"></AboutCard>
+        <AboutCard layout="right" content="Our team collectively has 20 year of.." header="built on extensive experience in financial insurance and reinsurance"></AboutCard>
+        <AboutCard layout="left" content="Our team collectively has 20 year of.." header="our approach"></AboutCard>
       </div>
       <div className="Border"></div>
       <Footer></Footer>
